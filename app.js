1772e01d8b42d6610a0ba8eba5a74473b2b4d270
Amandine Leporace <leporace.amandine@gmail.com>
2022-11-15 10:57:28 +0100

    Insérer des utilisateurs dans la base de données

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json()); // add this line
+
+const port = process.env.APP_PORT ?? 5001;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUsers);
+
+
+
+
